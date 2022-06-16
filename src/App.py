67368d92529663876bd1c3fc1b66c8175b166385
Leandro Oliveira <lmfo2103@outlook.com>
2022-06-15 21:55:44 -0300

    Quarta

--- src/App.py
+++ src/App.py
@@ -2,18 +2,23 @@ import pandas
 import networkx
 import matplotlib.pyplot as plt
 
-path = r"dados\fiscalizacao_eletronica_jan2021.csv"
-dataFrame = pandas.read_csv(path)
-print(dataFrame.head(2))
+# path = r"dados\fiscalizacao_eletronica_jan2021.csv"
+# dataFrame = pandas.read_csv(path)
+# # print(dataFrame)
 
 graph = networkx.Graph()
 graph.add_node(1)
-graph.add_nodes_from([2, 3, 4])
+graph.add_nodes_from([
+    (2, {"color": "red"}),
+    (3, {"color": "green"})
+])  
 graph.add_edge(1, 2)
-graph.add_edges_from([(2, 3), (3, 4)])
+# graph.add_edges_from([(2, 3), (3, 4)])
 # print(graph)
 node_list = graph.nodes()
 # print(node_list)
 
+graph[1]
+
 networkx.draw(graph, with_labels=True, font_weight='bold')
-plt.show()  
+plt.show()
